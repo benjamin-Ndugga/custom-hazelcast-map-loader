a9189107f31d09e085445902218641bf414f08e5
Benjamin Ndugga <>
2020-02-20 18:21:26 +0300

    excluded netbeans specific files from being tracked

---
+++ src/main/java/com/hazelcast/custom/Client.java
@@ -0,0 +1,22 @@
+package com.hazelcast.custom;
+
+
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+/**
+ *
+ * @author Benjamin E Ndugga
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient();
+        IMap<Object, Object> map = hazelcastInstance.getMap("capitals");
+        map.put("UGANDA", "KAMPALA");
+        System.out.println("returned value from store: " + map.get("UGANDA"));
+        hazelcastInstance.shutdown();
+    }
+}
---
+++ src/main/java/com/hazelcast/custom/Sever.java
@@ -0,0 +1,20 @@
+package com.hazelcast.custom;
+
+
+
+
+
+import static com.hazelcast.core.Hazelcast.newHazelcastInstance;
+import com.hazelcast.core.HazelcastInstance;
+
+/**
+ *
+ * @author Benjamin E Ndugga
+ */
+public class Sever {
+
+    public static void main(String[] args) {
+        HazelcastInstance hazelcastInstance = newHazelcastInstance();
+    }
+
+}
